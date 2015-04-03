6bb6359d7c68993e0893ca5c89fb1a09fa5ce98d
Sehwook <seh.wook.lee@cern.ch>
2015-04-04 03:17:26 +0900

    Changed cut values

--- PFHadHLT/python/PFHadCalib_cfg.py
+++ PFHadHLT/python/PFHadCalib_cfg.py
@@ -7,6 +7,7 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 #process.load("PFHadCalib.PFHadHLT.hlt_testJetsMET_Method3_keepAllCollectionsForPFHadronCalibration")
 
 from PFHadCalib.PFHadHLT.hlt_testJetsMET_Method3_keepAllCollectionsForPFHadronCalibration import *
+
 #process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 #process.source = cms.Source("PoolSource",
@@ -20,14 +21,14 @@ process.pfhadhlt = cms.EDAnalyzer('PFHadHLT',
                    HLTPFCandidates  = cms.InputTag("hltParticleFlow","","TEST"),
                    PFSimParticles   = cms.InputTag("particleFlowSimParticle","","TEST"),
 
-                   ptMin = cms.double(1.),                     # Minimum pt
-                   pMin = cms.double(3.),                      # Minimum p
+                   ptMin = cms.double(0.01),                     # Minimum pt
+                   pMin = cms.double(0.01),                      # Minimum p
                    nPixMin = cms.int32(2),                     # Nb of pixel hits
                    #nHitMin = cms.vint32(14,17,20,17),          # Nb of track hits
-                   nHitMin = cms.vint32(9,9,9,9),          # Nb of track hits
-                   nEtaMin = cms.vdouble(1.4, 1.6, 2.0, 2.4),  # in these eta ranges
-                   hcalMin = cms.double(1.),                   # Minimum hcal energy
-                   ecalMax = cms.double(0.2),                  # Maximum ecal energy 
+                   nHitMin = cms.vint32(8, 8, 8, 8),          # Nb of track hits
+                   nEtaMin = cms.vdouble(1.4, 1.6, 2.0, 2.5),  # in these eta ranges
+                   hcalMin = cms.double(0.0),                   # Minimum hcal energy
+                   ecalMax = cms.double(1e12),                  # Maximum ecal energy 
                    rootOutputFile = cms.string('PFHadCalibration.root')
 )
 
